5f012d71933a93af17376e4ad4c73abce79b9da8
Jack <77440283+DmlTheDev@users.noreply.github.com>
2022-02-21 19:56:11 +0000

    Delete events directory

--- events/ready.js
+++
@@ -1,5 +0,0 @@
-const chalk = require("chalk")
-const config = require("../cfg/config.json")
-module.exports = (client) => {
-    console.log(chalk.magenta(`Dmls Helper  by dml#4665 \nPrefix is ${config.prefix}`));
-  }
